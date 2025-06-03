50e5e58782252d48a46d9aa92bbdb0ec98d9229f
Sahan <sahan.toksoz@student.heaj.be>
2025-06-03 13:48:15 +0200

    CSS main portfolio version mobile test

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,31 @@
 "use strict";
 
 
+  document.addEventListener("DOMContentLoaded", function () {
+    const toggle = document.querySelector(".navbar-toggle");
+    const menu = document.querySelector(".navbar-list");
+    const links = document.querySelectorAll(".navbar-links a");
+
+    toggle.addEventListener("click", function () {
+      toggle.classList.toggle("active");
+      menu.classList.toggle("active");
+    });
+
+
+    links.forEach(link => {
+      link.addEventListener("click", function () {
+        toggle.classList.remove("active");
+        menu.classList.remove("active");
+      });
+    });
+  });
+
+
+
+
+
+
+
+
             
   
